099c8babbe230cd69549ed4f3eb8afd8c9f42fd4
johnLamberts <faithsacredoo3@gmail.com>
2024-03-29 14:26:36 +0800

    add request-object interface

---
+++ src/interface/CustomTypes.ts
@@ -0,0 +1,23 @@
+import { Request } from "express";
+
+export interface IRequestObject {
+  type: string;
+  description: string;
+  url: string;
+}
+
+export interface TPaginationRequest extends Request {
+  query: {
+    limit: string;
+    page: string;
+    orderBy: string;
+    sortBy: string;
+    filterBy: string;
+    category: string;
+    search: string;
+    content: string;
+    role: string;
+    sort: string;
+    fields: string;
+  };
+}
