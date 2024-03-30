682ed91f67ed991068a0a1552a1a1e5780c6358d
johnLamberts <faithsacredoo3@gmail.com>
2024-03-30 16:36:11 +0800

    fixed: two import which resides to obselete

--- src/interface/ErrorResponse.ts
+++ src/interface/ErrorResponse.ts
@@ -1,7 +1,5 @@
-import IResponse from "./CustomResponse";
+import { IResponse } from "./CustomResponse";
 
 export interface IErrorResponse extends IResponse {
   stack?: string;
 }
-
-export default IErrorResponse;
