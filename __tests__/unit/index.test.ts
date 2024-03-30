528f259e06d21725f5412cd4132de1908f5ba4ba
johnLamberts <faithsacredoo3@gmail.com>
2024-03-30 18:03:31 +0800

    feat: add test for utils

--- __tests__/unit/index.test.ts
+++ __tests__/unit/index.test.ts
@@ -1,3 +1,5 @@
+import getImageExtension from "~/utils/getImageExtension";
+
 describe("Testing to see if Jest works", () => {
   describe("given two plus two", () => {
     it("should return 4", () => {
@@ -9,6 +11,16 @@ describe("Testing to see if Jest works", () => {
   describe("given true === true", () => {
     it("should return true", () => {
       expect(true).toBe(true);
+    });
+  });
+});
+
+describe("utils", () => {
+  describe("getImageExtension", () => {
+    describe('given the mimetype "image/png"', () => {
+      it("should return .png", () => {
+        expect(getImageExtension("image/png")).toMatch(/.png/);
+      });
     });
   });
 });
